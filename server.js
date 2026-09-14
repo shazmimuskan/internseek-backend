e68cabbb8b67342367670ef691163d5a3032a4f1
muskan-demo <shazmimuskan@gmail.com>
2026-09-14 21:29:38 +0530

    Add request logging

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ const Application = require("./models/Application");
 
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log("REQUEST:", req.method, req.url, req.body);
+  next();
+});
 
 /* -------------------- GROQ SETUP -------------------- */
 const groq = new Groq({
